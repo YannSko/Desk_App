62513575830e0044a31323116d48bb4582ec8095
Yann Sko <yskonieczny@gmail.com>
2024-02-22 21:45:57 +0100

    FIN_CRUD
    CA CRUD A MAX :
    Le user a  totale liberté de update et delete . il peut delete 1 ou plusieurs elements precis ( table, colonne, row ) . de meme dans le update

--- Data/Database/database.py
+++ Data/Database/database.py
@@ -3,25 +3,33 @@ from database_utils import *
 import time
 
 
-
+update_data()
+update_data()
+update_data()
+#delete_data()
+#delete_data()
+#delete_data()
 
 
 # Create table using the JSON input
-create_table_from_json()
+#create_table_from_json()
 
 # Insert nested data into the table
-insert_nested_data()
+#insert_nested_data()
 
-time.sleep(2)
+#time.sleep(2)
 # Retrieve and print data from the table
-data = get_data_x()
-print("Retrieved data:", data)
+#data = get_data_x()
+#print("Retrieved data:", data)
 
-data2 = get_data_x()
-print("Retrieved data:", data2)
-data3 = get_data_x()
-print("Retrieved data:", data3)
+#data2 = get_data_x()
+#print("Retrieved data:", data2)
+#data3 = get_data_x()
+#print("Retrieved data:", data3)
 
+#update_data()
+#update_data()
+#update_data()
 
 
 
--- Data/Database/database_utils.py
+++ Data/Database/database_utils.py
@@ -85,7 +85,7 @@ def check_and_drop(table_name):
                 # Vérifier chaque colonne
                 for column_name in column_names:
                     # Sélectionner 10 lignes aléatoires de la colonne
-                    cur.execute("SELECT {} FROM {} TABLESAMPLE SYSTEM(50)".format(column_name, table_name))
+                    cur.execute("SELECT {} FROM {} TABLESAMPLE SYSTEM(70)".format(column_name, table_name))
                     values = [row[0] for row in cur.fetchall()]
 
                     # Vérifier si toutes les valeurs sont nulles
@@ -261,35 +261,218 @@ def get_data_x():
 
 
 
-def update_data( id, data):
-    table_name = input("Enter the table name: ")
-    while not isinstance(table_name, str):
-        print("Table name must be a string.")
-        table_name = input("Enter the table name: ")
+def update_data():
     conn = connect_to_database()
     if conn:
         try:
             with conn.cursor() as cur:
-                # Sanitize column names and values
-                set_values = ", ".join(["{} = %s".format(sanitize_column_name(key)) for key in data.keys()])
-                values = tuple(data.values()) + (id,)
-                cur.execute("UPDATE {} SET {} WHERE id = %s;".format(table_name, set_values), values)
-                conn.commit()
-                print("Data updated successfully.")
+                print("Options disponibles :")
+                print("1. Modifier le nom d'une table.")
+                print("2. Modifier le nom d'une colonne.")
+                print("3. Modifier la valeur d'une cellule dans une colonne.")
+
+                choice = input("Entrez le numéro correspondant à votre choix : ")
+
+                if choice == '1':
+                    # Modifier le nom d'une table
+                    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                    tables = [row[0] for row in cur.fetchall()]
+                    print("Tables disponibles :")
+                    for table in tables:
+                        print(table)
+
+                    old_table_name = input("Entrez le nom de la table à modifier : ")
+                    new_table_name = input("Entrez le nouveau nom pour la table : ")
+
+                    if old_table_name in tables:
+                        cur.execute("ALTER TABLE {} RENAME TO {};".format(old_table_name, new_table_name))
+                        print("Le nom de la table {} a été modifié avec succès en {}.".format(old_table_name, new_table_name))
+                        conn.commit()
+                    else:
+                        print("La table spécifiée n'existe pas.")
+                    
+                elif choice == '2':
+                    # Modifier le nom d'une colonne
+                    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                    tables = [row[0] for row in cur.fetchall()]
+                    print("Tables disponibles :")
+                    for table in tables:
+                        print(table)
+                    table_name = input("Entrez le nom de la table : ")
+
+                    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s;", (table_name,))
+                    columns = [row[0] for row in cur.fetchall()]
+                    print("Colonnes disponibles dans la table {} :".format(table_name))
+                    for column in columns:
+                        print(column)
+
+                    old_column_name = input("Entrez le nom de la colonne à modifier : ")
+                    new_column_name = input("Entrez le nouveau nom pour la colonne : ")
+
+                    if old_column_name in columns:
+                        cur.execute("ALTER TABLE {} RENAME COLUMN {} TO {};".format(table_name, old_column_name, new_column_name))
+                        print("Le nom de la colonne {} de la table {} a été modifié avec succès en {}.".format(old_column_name, table_name, new_column_name))
+                        conn.commit()
+                    else:
+                        print("La colonne spécifiée n'existe pas dans la table {}.".format(table_name))
+
+                elif choice == '3':
+                    # Modifier la valeur d'une cellule dans une colonne
+                    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                    tables = [row[0] for row in cur.fetchall()]
+                    print("Tables disponibles :")
+                    for table in tables:
+                        print(table)
+                    table_name = input("Entrez le nom de la table : ")
+
+                    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s;", (table_name,))
+                    columns = [row[0] for row in cur.fetchall()]
+                    print("Colonnes disponibles dans la table {} :".format(table_name))
+                    for column in columns:
+                        print(column)
+
+                    column_name = input("Entrez le nom de la colonne à modifier : ")
+                    id_to_update = int(input("Entrez l'ID de la ligne à modifier : "))
+
+                    if column_name in columns:
+                        cur.execute("SELECT {} FROM {} WHERE id = %s;".format(column_name, table_name), (id_to_update,))
+                        old_value = cur.fetchone()[0]
+                        print("La valeur actuelle de la cellule dans la colonne {} et l'ID {} est : {}".format(column_name, id_to_update, old_value))
+                        new_value = input("Entrez la nouvelle valeur pour la cellule : ")
+
+                        # Vérifier la compatibilité du type de valeur avec le type de la colonne
+                        cur.execute("SELECT data_type FROM information_schema.columns WHERE table_name = %s AND column_name = %s;", (table_name, column_name))
+                        column_data_type = cur.fetchone()[0]
+
+                        if column_data_type == 'integer':
+                            try:
+                                new_value = int(new_value)
+                            except ValueError:
+                                print("La colonne '{}' accepte uniquement des valeurs entières.".format(column_name))
+                                return
+                        elif column_data_type == 'numeric':
+                            try:
+                                new_value = float(new_value)
+                            except ValueError:
+                                print("La colonne '{}' accepte uniquement des valeurs numériques.".format(column_name))
+                                return
+
+                        cur.execute("UPDATE {} SET {} = %s WHERE id = %s;".format(table_name, column_name), (new_value, id_to_update))
+                        print("La valeur de la cellule dans la colonne {} et l'ID {} a été modifiée avec succès.".format(column_name, id_to_update))
+                        conn.commit()
+                    else:
+                        print("La colonne spécifiée n'existe pas dans la table {}.".format(table_name))
+
+                else:
+                    print("Choix invalide. Veuillez choisir une option valide (1, 2 ou 3).")
+
         except Exception as error:
             print(error)
         finally:
             conn.close()
 
-def delete_data(table_name, id):
-    conn = connect_to_database()
-    if conn:
-        try:
-            with conn.cursor() as cur:
-                cur.execute("DELETE FROM {} WHERE id = %s;".format(table_name), (id,))
-                conn.commit()
-                print("Data deleted successfully.")
-        except Exception as error:
-            print(error)
-        finally:
-            conn.close()
+
+def delete_data():
+    print("Options disponibles :")
+    print("1. Supprimer une ou plusieurs tables spécifiques.")
+    print("2. Supprimer une ou plusieurs colonnes spécifiques d'une table.")
+    print("3. Supprimer une ou plusieurs lignes spécifiques d'une table en fonction de leur ID.")
+
+    choice = input("Entrez le numéro correspondant à votre choix : ")
+
+    if choice == '1':
+        # Supprimer une ou plusieurs tables spécifiques
+        conn = connect_to_database()
+        if conn:
+            try:
+                with conn.cursor() as cur:
+                    # Récupérer les noms de toutes les tables disponibles
+                    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                    tables = [row[0] for row in cur.fetchall()]
+                    print("Tables disponibles :")
+                    for table in tables:
+                        print(table)
+
+                    table_names = input("Entrez les noms des tables à supprimer, séparés par des virgules : ").split(',')
+                    for table_name in table_names:
+                        if table_name.strip() not in tables:
+                            print("La table '{}' n'existe pas.".format(table_name.strip()))
+                            return
+
+                    for table_name in table_names:
+                        cur.execute("DROP TABLE IF EXISTS {};".format(table_name.strip()))
+                        print("La table {} a été supprimée avec succès.".format(table_name.strip()))
+                    conn.commit()
+            except Exception as error:
+                print("Erreur lors de la suppression des tables :", error)
+            finally:
+                conn.close()
+
+    elif choice == '2':
+        # Supprimer une ou plusieurs colonnes spécifiques d'une table
+        conn = connect_to_database()
+        if conn:
+            try:
+                with conn.cursor() as cur:
+                    # Récupérer les noms de toutes les tables disponibles
+                    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                    tables = [row[0] for row in cur.fetchall()]
+                    print("Tables disponibles :")
+                    for table in tables:
+                        print(table)
+                    table_name = input("Entrez le nom de la table : ")
+                    # Récupérer les noms de toutes les colonnes de la table spécifiée
+                    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s;", (table_name,))
+                    columns = [row[0] for row in cur.fetchall()]
+                    print("Colonnes disponibles dans la table {} :".format(table_name))
+                    for column in columns:
+                        print(column)
+
+                    column_names = input("Entrez les noms des colonnes à supprimer, séparés par des virgules : ").split(',')
+                    for column_name in column_names:
+                        if column_name.strip() not in columns:
+                            print("La colonne '{}' n'existe pas dans la table '{}'.".format(column_name.strip(), table_name))
+                            return
+
+                    for column_name in column_names:
+                        cur.execute("ALTER TABLE {} DROP COLUMN {};".format(table_name, column_name.strip()))
+                        print("La colonne {} de la table {} a été supprimée avec succès.".format(column_name.strip(), table_name))
+                    conn.commit()
+            except Exception as error:
+                print("Erreur lors de la suppression des colonnes :", error)
+            finally:
+                conn.close()
+
+    elif choice == '3':
+        # Supprimer une ou plusieurs lignes spécifiques d'une table en fonction de leur ID
+        conn = connect_to_database()
+        if conn:
+            try:
+                with conn.cursor() as cur:
+                    # Récupérer les noms de toutes les tables disponibles
+                    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                    tables = [row[0] for row in cur.fetchall()]
+                    print("Tables disponibles :")
+                    for table in tables:
+                        print(table)
+                    table_name = input("Entrez le nom de la table : ")
+                    # Récupérer les noms de toutes les colonnes de la table spécifiée
+                    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s;", (table_name,))
+                    columns = [row[0] for row in cur.fetchall()]
+                    print("Colonnes disponibles dans la table {} :".format(table_name))
+                    for column in columns:
+                        print(column)
+
+                    ids = input("Entrez les ID des lignes à supprimer, séparés par des virgules : ").split(',')
+                    for id in ids:
+                        cur.execute("DELETE FROM {} WHERE id = %s;".format(table_name), (id.strip(),))
+                        print("La ligne avec l'ID {} de la table {} a été supprimée avec succès.".format(id.strip(), table_name))
+                    conn.commit()
+            except Exception as error:
+                print("Erreur lors de la suppression des lignes :", error)
+            finally:
+                conn.close()
+
+    else:
+        print("Choix invalide. Veuillez choisir une option valide (1, 2 ou 3).")
+
